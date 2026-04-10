02c60302519cf9948277a9ee1e3f7552e1e8fa6d
Sam Carlberg <sam@slfc.dev>
2026-04-09 20:05:42 -0400

    [cmd3] Enforce command lifetimes across all opmode and command scopes (#8705)
    Commands are no longer able to outlive their schedule-site's scope,
    regardless of how they were scheduled (set as a default command, bound
    to a trigger, or manually scheduled)
    
    As a consequence, default commands need better tracking so the default
    command setting can be released when their scope exits and the next-most
    appropriate default command can be rescheduled (eg, an opmode sets a
    default command, then the globally-scoped default is restored when the
    opmode exits). Some complexity is required here to make it work well for
    edge cases.
    
    Like `schedule()`, `setDefaultCommand()` will immediately start the new
    default command if called inside of another command to avoid 1-loop
    delays. However, this does not apply when called by the _current_
    default command, as it would result in attempting to cancel the default
    command while it's mounted (which is impossible and would throw an
    exception)
    
    ```java
    class Robot extends OpModeRobot {
      final Drive drive = new Drive();
      final CommandXboxController controller = new CommandXboxController(1);
    
      public Robot() {
        // global default command, active unless overridden in an opmode or command
        drive.setDefaultCommand(drive.stop());
    
        // global trigger binding, always active
        controller.rightBumper().onTrue(drive.setX());
      }
    }
    
    @Teleop
    class ExampleOpMode extends PeriodicOpMode {
      public ExampleOpMode(Robot robot) {
        // opmode-specific default command
        robot.drive.setDefaultCommand(robot.drive.operatorControl(robot.controller));
    
        // opmode-specific binding
        robot.controller.leftBumper().whileTrue(robot.drive.stop());
    
        // opmode-specific binding that takes precedence over the global binding
        // because it happens last; it "wins out" over the `setX()` binding
        robot.controller.rightBumper().onTrue(robot.drive.selfTest());
      }
    
      @Override
      public void periodic() {
        Scheduler.getDefault().run();
      }
    }
    ```

--- wpilibc/src/main/native/cpp/driverstation/DriverStation.cpp
+++ wpilibc/src/main/native/cpp/driverstation/DriverStation.cpp
@@ -159,6 +159,7 @@ struct Instance {
   // Op mode lookup
   wpi::util::mutex opModeMutex;
   wpi::util::DenseMap<int64_t, HAL_OpModeOption> opModes;
+  bool userProgramStarted = false;
 
   wpi::units::second_t nextMessageTime = 0_s;
 
@@ -679,7 +680,24 @@ void DriverStation::ClearOpModes() {
   HAL_SetOpModeOptions(nullptr, 0);
 }
 
+void DriverStation::ObserveUserProgramStarting() {
+  ::GetInstance().userProgramStarted = true;
+  HAL_ObserveUserProgramStarting();
+}
+
+int64_t DriverStation::GetOpModeId() {
+  if (!::GetInstance().userProgramStarted) {
+    return 0;
+  }
+
+  return GetControlWord().GetOpModeId();
+}
+
 std::string DriverStation::GetOpMode() {
+  if (!::GetInstance().userProgramStarted) {
+    return 0;
+  }
+
   return GetInstance().OpModeToString(GetOpModeId());
 }
 
